0e225985d581e8cd46746622f7cd5530bf3d7508
filiks <runlevel07@gmail.com>
2019-12-28 11:52:57 +0100

    react livecycle orders

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+  // 1. constructor
+  constructor(props) {
+    super(props)
+
+    this.state = { name: 'francis'}
+    console.log('1 - constructor');
+  }
+
+  //2. static getDerivedStateFromProps
+  static getDerivedStateFromProps(){
+    console.log('2 - getDerivedStateFromProps');
+    return null
+  }
+
   render() {
+    console.log('3 - render will be executed as 3rd');
     return (
       <div>Home</div>
     )
   }
+
+  componentDidMount() {
+    console.log('4 - componentDidMount');
+  }
+
+  componentWillUnmount() {
+    console.log('5 - only if component will unmount from dom');
+  }
+
 }
 
 
