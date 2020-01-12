57491966df519e31dc317a0cdeb0e15ea782dba9
filiks <runlevel07@gmail.com>
2020-01-12 20:23:17 +0100

    hooks with fetch and a counter

--- src/components/hooks.js
+++ src/components/hooks.js
@@ -1,6 +1,7 @@
 import React,  {useEffect, useState } from 'react';
 import { useForm } from './useForm';
 import Hello from './Hello';
+import useFetch from './useFetch'
 
 const Hooks = () => {
   const [values, handleChange] = useForm({
@@ -17,10 +18,20 @@ const Hooks = () => {
       }
     }, [values.email])
 
+    // showHello state
     const [showHello, setShowHello] = useState(false);
+
+    //counter - starting with 0
+    const [count, setCount] = useState(0);
+
+    // useFetch
+    const {data, loading} = useFetch(`http://numbersapi.com/${count}/trivia`)
 
   return(
     <div>
+    <div>{loading ? "loading...." : data}</div>
+    <div>count: {count}</div>
+    <button onClick={()=> setCount(c => c + 1)}>increment</button>
       <>
         <h2>hooks Form</h2>
         <button onClick={() => setShowHello(!showHello)}>toggle Hello </button>
